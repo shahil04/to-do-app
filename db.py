66b075f25423e061b7ca237eacffbb3d99afe2af
md shahil ansari <ansarirahish455@gmail.com>
2025-07-23 17:49:15 +0530

    add wsgi file

--- db.py
+++ db.py
@@ -25,21 +25,18 @@
 #     init_db()
 
 
+import mysql.connector
 
+from secret_key import *
 
-
-# import mysql.connector
-
-# from secret_key import *
-
-# def get_db_connection():
-#     connection = mysql.connector.connect(
-#         host     ="localhost",   # Your MySQL host
-#         user     =db_username,   # Your MySQL username
-#         password =db_password,   # Your MySQL password
-#         database =db_name        # Database name
-#     )
-#     return connection
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host     ="34.67.229.167",   # Your MySQL host
+        user     =db_username,   # Your MySQL username
+        password =db_password,   # Your MySQL password
+        database =db_name        # Database name
+    )
+    return connection
 
 
 
--- secret_key.py
+++ secret_key.py
@@ -1,3 +1,8 @@
-db_username= "root"
+db_username= "root2"
 db_password = "Azsxdcf123@"
 db_name = "todo_db"
+
+
+# db_username= "root"
+# db_password = "Azsxdcf123@"
+# db_name = "todo_db"
